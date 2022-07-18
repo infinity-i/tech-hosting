dcf1fab24097780490494e4d5613b56c7bd38d6e
Ashik cv <ashikcv17@gmail.com>
2022-07-18 08:32:34 +0530

    welcome page in progress, section2 completed successfully...

--- FrontEnd/src/app/animations.ts
+++ FrontEnd/src/app/animations.ts
@@ -31,10 +31,18 @@ export let fadeZ = trigger('fadeZ', [
     ])
   ])
 
-// export let imgani = trigger('imgani', [
+export let imgani = trigger('imgani', [
 
-//     state('void', style({opacity:0,transform: 'translateY(110px)'})),
-//     transition(':landing', [ //void <=> *
-//       animate('1s 1000ms ease-out')
-//     ])
-//   ])
+    state('void', style({opacity:1,display:'hidden',transform: 'translateY(550px)'})),
+    transition(':enter', [ //void <=> *
+      animate('.65s 1000ms ease-out')
+    ])
+  ])
+
+  export let imgani1 = trigger('imgani1', [
+
+    state('void', style({opacity:1,transform: 'translateY(350px)'})),
+    transition(':enter', [ //void <=> *
+      animate('.55s 1210ms ease-out')
+    ])
+  ])
--- FrontEnd/src/app/welcome/welcome.component.ts
+++ FrontEnd/src/app/welcome/welcome.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { downA, fadeL, fadeR, fadeZ } from '../animations';
+import { downA, fadeL, fadeR, fadeZ, imgani, imgani1 } from '../animations';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css'],
   animations: [
-    fadeR , fadeL , fadeZ , downA 
+    fadeR , fadeL , fadeZ , downA , imgani , imgani1
 
   ]
 })
