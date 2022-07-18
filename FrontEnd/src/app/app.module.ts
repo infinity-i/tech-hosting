71a86cb1be0fc94fdd855b77875dcf5b9358b596
Ajithvc45 <ajithvc45@gmail.com>
2022-07-18 12:19:00 +0530

    formsmodule added

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import {BrowserAnimationsModule} from'@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +20,8 @@ import { SignupComponent } from './signup/signup.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
