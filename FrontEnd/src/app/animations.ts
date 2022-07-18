4b18629eae36c0f6fc19f0d72e4e0367333cfb35
Ashik cv <ashikcv17@gmail.com>
2022-07-18 18:46:52 +0530

    section3 successfully completed...

--- FrontEnd/src/app/animations.ts
+++ FrontEnd/src/app/animations.ts
@@ -33,7 +33,7 @@ export let fadeZ = trigger('fadeZ', [
 
 export let imgani = trigger('imgani', [
 
-    state('void', style({opacity:1,display:'hidden',transform: 'translateY(550px)'})),
+    state('void', style({display:'hidden',transform: 'translateY(550px)'})),
     transition(':enter', [ //void <=> *
       animate('.65s 1000ms ease-out')
     ])
@@ -44,5 +44,37 @@ export let imgani = trigger('imgani', [
     state('void', style({opacity:1,transform: 'translateY(350px)'})),
     transition(':enter', [ //void <=> *
       animate('.55s 1210ms ease-out')
+    ])
+  ])
+
+  export let letter1 = trigger('letter1', [
+
+    state('void', style({display:'hidden',transform: 'translateX(-700px)'})),
+    transition(':enter', [ //void <=> *
+      animate('.65s 1000ms ease-out')
+    ])
+  ])
+
+  export let letter2 = trigger('letter2', [
+
+    state('void', style({display:'hidden',transform: 'translateX(-600px)'})),
+    transition(':enter', [ //void <=> *
+      animate('.85s 1000ms ease-out')
+    ])
+  ])
+
+  export let letter3 = trigger('letter3', [
+
+    state('void', style({display:'hidden',transform: 'translateX(-700px)'})),
+    transition(':enter', [ //void <=> *
+      animate('1.15s 1000ms ease-out')
+    ])
+  ])
+
+  export let letter4 = trigger('letter4', [
+
+    state('void', style({display:'hidden',transform: 'translateX(-600px)'})),
+    transition(':enter', [ //void <=> *
+      animate('.55s 1000ms ease-out')
     ])
   ])
