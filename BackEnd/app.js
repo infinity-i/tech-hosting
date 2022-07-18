92122ff2119cee1e5e716c5f851b9a20004af4e1
Steffi Job <steffijob96@gmail.com>
2022-07-18 21:49:44 +0530

    added authorization

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -112,3 +112,27 @@ app.get('/admin/approve', function(req,res){
 app.listen(process.env.PORT,()=>{
     console.log(`Server up and running in ${process.env.PORT}`);
 })
+
+
+//authorization
+
+function verifyToken(req,res,next)
+{
+    if(!req.headers.authorization)
+    {
+        return res.status(401).send('Unauthorized request')
+    }
+    let token = req.headers.authorization.split('')[1]
+    if (token =='null')
+    {
+        return res.status(401).send('Unauthorized request')
+    }
+    let payload= jwt.verify(token,'secretkey')
+    console.log(payload)
+    if(!payload)
+    {
+        return res.status(401).send('Unauthorized request')
+    }
+    req.userId=payload.subject
+    next()
+}
