52957e1de6858d4d80077bc364bf50bebf4bbd06
Ajithvc45 <ajithvc45@gmail.com>
2022-07-22 14:12:53 +0530

    admin page

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from './auth.service';
+import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import {BrowserAnimationsModule} from'@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +11,10 @@ import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { SignupComponent } from './signup/signup.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { AdminComponent } from './admin/admin.component';
+import { PendingComponent } from './pending/pending.component';
+
 
 @NgModule({
   declarations: [
@@ -16,16 +22,20 @@ import { FooterComponent } from './footer/footer.component';
     LoginComponent,
     WelcomeComponent,
     SignupComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    AdminComponent,
+    PendingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
