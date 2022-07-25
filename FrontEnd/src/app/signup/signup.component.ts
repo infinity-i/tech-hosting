edf526d3ca68214081fb1db386dfb5042df34a22
akhilasreeni <akhilasreenivasan66@gmail.com>
2022-07-25 11:56:04 +0530

    error fixed

--- FrontEnd/src/app/signup/signup.component.ts
+++ FrontEnd/src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -8,22 +9,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  user = {
-    fullName:'',
-    email:'',
-    phoneNo:'',
-    password:'',
-    repeatPassword:''
-  }
+  signupForm: FormGroup | any;
+  
+  // user = {
+  //   fullName:'',
+  //   email:'',
+  //   phoneNo:'',
+  //   password:'',
+  //   repeatPassword:''
+  // }
  
   constructor(private authservice:AuthService, private router:Router) { }
 
   ngOnInit(): void{
-
+    this.signupForm = new FormGroup({
+      fullName:new FormControl (null,Validators.required ),
+      email:new FormControl (null,[Validators.email,Validators.required]),
+      phoneNo:new FormControl (null,Validators.required),
+      userType:new FormControl(null,Validators.required),
+      password: new FormControl (null, Validators.required),
+      repeatPassword: new FormControl(null,Validators.required)
+    })  
   }
-  onsubmit(){
+  onsubmit(signupForm: any){
     console.log("register button hit");
-    this.authservice.CreateUser(this.user);
+    this.authservice.CreateUser(this.signupForm.value);
     alert('User Registration successfull');
     this.router.navigate(['login']);
   }
