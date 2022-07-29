4aba816a5475eff01b6fcb13060ecbbe058547dd
Ashik cv <ashikcv17@gmail.com>
2022-07-29 12:35:21 +0530

    completed the view blog design

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -33,7 +33,7 @@ const routes: Routes = [{path:'',component:WelcomeComponent},
     },
     
     {
-      path:'back',component:HomeComponent
+      path:'back',component:BlogsComponent
     },
 
   ]},
