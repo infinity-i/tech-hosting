157233bca6190e4066a057f377685183d2e0ab1d
Steffi Job <steffijob96@gmail.com>
2022-07-29 23:20:06 +0530

    modified file

--- FrontEnd/src/app/login/login.component.ts
+++ FrontEnd/src/app/login/login.component.ts
@@ -28,15 +28,31 @@ onlogin() {
     console.log("login button hit");
     console.log(this.loginUserData);
     
-    this.auth.loginUser(this.loginUserData)
-    .subscribe(
-      (      res: { token: string; }) => {
-        localStorage.setItem('token', res.token)
-        this.router.navigate(['/home'])
-      },
-      (      err: any) => {
-        console.log(err);
-        this.router.navigate(['/welcome'])
-      });
+    // this.auth.loginUser(this.loginUserData)
+    // .subscribe(
+    //   (      res: { token: string; }) => {
+    //     localStorage.setItem('token', res.token)
+    //     this.router.navigate(['/home'])
+    //   },
+    //   (      err: any) => {
+    //     console.log(err);
+    //     this.router.navigate(['/welcome'])
+    //   });
+
+    if(this.loginUserData.email==null||this.loginUserData.password==null){
+      console.log("values missing");
+      this.router.navigate(['login'])
+    }
+    else{
+      this.auth.loginUser(this.loginUserData)
+        .subscribe((res: { token: string; }) => {
+            localStorage.setItem('token', res.token)
+            this.router.navigate(['/home'])
+          },
+          (     err: any) => {
+            console.log(err);
+            this.router.navigate(['login'])
+          });
+    }  
   }
 }
