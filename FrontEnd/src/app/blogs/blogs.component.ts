b1e85ad585448cf2479370d02181507fb7daa60f
Ashik cv <ashikcv17@gmail.com>
2022-08-04 00:35:21 +0530

    filtering has been completed successfully....

--- FrontEnd/src/app/blogs/blogs.component.ts
+++ FrontEnd/src/app/blogs/blogs.component.ts
@@ -17,11 +17,26 @@ export class BlogsComponent implements OnInit {
 
   latestlog:BlogModel[];
 
+  _filtervalue: string = '';
+
+  filterblog: BlogModel[];
+
+  get filtervalue(){
+    return this._filtervalue;
+  }
+
+  set filtervalue(value:string){
+    this._filtervalue = value;
+    this.filterblog = this.filterBlogByCat(value);
+
+  }
+
   constructor(private blogService:BlogService, private router:Router) { }
 
   ngOnInit(): void {
     this.blogService.getapprovedBlogs().subscribe((data) => {
       this.blogs = JSON.parse(JSON.stringify(data));
+      this.filterblog = this.blogs;
     })
 
     this.blogService.getapprovedBloglatest().subscribe((data)=>{
@@ -33,6 +48,18 @@ export class BlogsComponent implements OnInit {
     localStorage.setItem("viewblog", i._id.toString());
     this.router.navigate(['home/viewblog']);
     // console.log("id = "+i._id)
+  }
+
+  filterBlogByCat(filterTerm:string){
+
+    if(this.blogs.length === 0 || this.filtervalue === ''){
+      return this.blogs;
+    } else {
+      return this.blogs.filter((blogs) => {
+        return blogs.category === filterTerm;
+      })
+    }
+
   }
 
 
